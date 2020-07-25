65f4b21de2c07302975ba8c16cb2bf9b1a5f5990
Gruszcat <gruszcat@gmail.com>
2020-07-25 15:08:06 +0000

    Initial commit

---
+++ People.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class People {
+  public static void main(String[] args) {
+    ArrayList<String> people = new ArrayList<String>();
+    people.add("employee");
+    people.add("sales lead");
+    people.add("customer");
+    people.add("ceo");
+    System.out.println(people);
+  }
+}
